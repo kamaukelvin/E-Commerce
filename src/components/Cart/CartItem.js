b79e20245b1b0eb8dbb44686ae01e93febd3b9f5
kamaukelvin <kevzkj.kk@gmail.com>
2020-01-29 16:34:28 +0300

    changed size of product in cart

--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -8,7 +8,7 @@ import React from 'react'
     return (
         <div className="row my-2 text-capitalize text-center">
             <div className="col-10 mx-auto col-lg-2">
-                <img src={img} style={{width:"5rem", height:"5rem"}} className="img-fluid" alt="product" />
+                <img src={img} style={{width:"8rem", height:"8rem"}} className="img-fluid" alt="product" />
             </div>
             <div className="col-10 mx-auto col-lg-2">
                <span className="d-lg-none">product: </span> {title}
@@ -19,9 +19,9 @@ import React from 'react'
             <div className="col-10 mx-auto col-lg-2 my-2 my-lg-0">
                 <div className="d-flex justify-content-center">
                     <div>
-                        <span className="btn btn-black mx-1" onClick={()=>decrement(id)}>-</span>
-                        <span className="btn btn-black mx-1">{count}</span>
-                        <span className="btn btn-black mx-1" onClick={()=>increment(id)}>+</span>
+                        <span className="btn btn-black mx-2" onClick={()=>decrement(id)}>-</span>
+                        <span className="btn btn-black mx-2">{count}</span>
+                        <span className="btn btn-black mx-2" onClick={()=>increment(id)}>+</span>
                     </div>
                 </div>
             </div>
